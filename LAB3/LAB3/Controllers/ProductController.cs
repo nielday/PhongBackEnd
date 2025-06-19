6490060aa109c497efde302db246e6dec02c6694
ĐÀO ĐỨC PHONG <162095979+nielday@users.noreply.github.com>
2025-06-19 09:17:17 +0700

    Add files via upload

---
+++ LAB3/LAB3/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using LAB3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LAB3.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Details()
+        {
+            // Truyền 1 sản phẩm demo
+            var product = new Product
+            {
+                Id = 101,
+                Name = "Laptop X1 Carbon",
+                Price = 2999.99M
+            };
+
+            return View(product);
+        }
+    }
+}
