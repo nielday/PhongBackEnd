3a61b66e27a4a1e5bc7422c475fcaf0c4fef97e3
ĐÀO ĐỨC PHONG <162095979+nielday@users.noreply.github.com>
2025-06-19 09:31:57 +0700

    Add files via upload

---
+++ LAB4/LAB4/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using LAB3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace LAB3.Controllers
+{
+    public class UserController : Controller
+    {
+        static List<User> users = new List<User>();
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(User user)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    users.Add(user);
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+            }
+
+            return View(user);
+        }
+
+        public IActionResult Index()
+        {
+            return View(users);
+        }
+    }
+}
---
+++ LAB4/LAB4/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LAB3.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Không được để trống!")]
+        [Display(Name = "Mã sinh viên")]
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "Không được để trống!")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Tài khoản từ 2 - 20 ký tự")]
+        [Display(Name = "Tài khoản")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Không được để trống!")]
+        [StringLength(10, MinimumLength = 6, ErrorMessage = "Mật khẩu từ 6 - 10 ký tự")]
+        [Display(Name = "Mật khẩu")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Không được để trống!")]
+        [Display(Name = "Điện thoại")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Không được để trống!")]
+        [EmailAddress(ErrorMessage = "Sai định dạng email")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
